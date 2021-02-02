91151988aee7cffd899b4c02b0d378abb261a2ef
zhangling <ling.zhang04@foxmail.com>
2021-02-02 18:02:41 +0800

    打印整数32位二进制

---
+++ primary/src/class01/Code06_PrintB.java
@@ -0,0 +1,36 @@
+package class01;
+
+/**
+ * 打印整数的32位 二进制表示
+ * 实际数的底层都是32位的
+ *
+ * @author zhangling 2021/2/2 17:04
+ */
+public class Code06_PrintB {
+
+    public static void print(int num) {
+        for (int i = 31; i >= 0; i--) {
+            // 1 << 5  == 100000，左移一位*2
+            // 1 & 1 == 1 ,其他都是0
+            // 如果num = 8,与1向左移动8位后&运算，第8位的1保留下来，其他位置都是0
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // int 32位，long 64位
+        int num = -1;
+        print(num);
+
+        System.out.println("================");
+        int a = 4;
+        int b = 12;
+        print(a);
+        print(b);
+        System.out.println("==============");
+        print(a & b);
+        print(a | b);
+        print(a ^ b);
+    }
+}

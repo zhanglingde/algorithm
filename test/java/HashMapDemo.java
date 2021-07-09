aa2c45235fa7110a35eb64b47da19413ce730613
zhangling <ling.zhang04@foxmail.com>
2021-07-09 17:50:17 +0800

    求数组小和

---
+++ test/java/HashMapDemo.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.function.BinaryOperator;
+
+/**
+ * @author zhangling 2021/7/9 15:41
+ */
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap<Integer, BigDecimal> map = new HashMap<>();
+        map.put(1, BigDecimal.ONE);
+        BigDecimal bigDecimal = map.get(1)
+                .add(null);
+        System.out.println("bigDecimal = " + bigDecimal);
+    }
+}

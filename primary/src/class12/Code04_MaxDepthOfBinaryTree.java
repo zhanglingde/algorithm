1040d96600b94e392ee52defc6038284adbc1b7f
zhangling <ling.zhang04@hand-china.com>
2021-09-07 23:39:27 +0800

    二叉树相关题目

---
+++ primary/src/class12/Code04_MaxDepthOfBinaryTree.java
@@ -0,0 +1,36 @@
+package class12;
+
+/**
+ * @author zhangling  2021/9/7 22:41
+ */
+public class Code04_MaxDepthOfBinaryTree {
+    public static class TreeNode {
+        int value;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.right.left = new TreeNode(3);
+        root.right.left.left = new TreeNode(3);
+
+
+        System.out.println("maxDepth(root) = " + maxDepth(root));
+
+    }
+}

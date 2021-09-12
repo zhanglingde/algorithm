2849d51453508521bce97ffdd5054b34e6cf9b0b
zhangling <ling.zhang04@hand-china.com>
2021-09-12 23:47:10 +0800

    二叉树序列化

---
+++ basic/src/class11/Code01_UnRecursiveTraversalBT.java
@@ -0,0 +1,98 @@
+package class11;
+
+import java.util.Stack;
+
+/**
+ * 非递归方式遍历二叉树
+ *
+ * @author zhangling  2021/9/12 16:30
+ */
+public class Code01_UnRecursiveTraversalBT {
+    public static class TreeNode {
+        int value;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int value) {
+            this.value = value;
+        }
+    }
+
+    // 先序
+    public static void pre(TreeNode head) {
+        if (head != null) {
+            Stack<TreeNode> stack = new Stack<>();
+            stack.add(head);
+            while (!stack.isEmpty()) {
+                TreeNode cur = stack.pop();
+                System.out.print(cur.value + " ");
+                if (cur.right != null) {
+                    stack.push(cur.right);
+                }
+                if (cur.left != null) {
+                    stack.push(cur.left);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    // 中序
+    public static void in(TreeNode head) {
+        if (head != null) {
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = head;
+            while (!stack.isEmpty() || cur != null) {
+                if (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                }else {
+                    cur = stack.pop();
+                    System.out.print(cur.value + " ");
+                    cur = cur.right;
+                }
+
+            }
+
+        }
+        System.out.println();
+    }
+
+    // 后序 头右左 -> 左右头
+    public static void pos(TreeNode head) {
+        if (head != null) {
+            Stack<TreeNode> stack = new Stack<>();
+            Stack<TreeNode> resStack = new Stack<>();
+            stack.add(head);
+            while (!stack.isEmpty()) {
+                TreeNode cur = stack.pop();
+                resStack.push(cur); // 头 右 左
+                if (cur.left != null) {
+                    stack.push(cur.left);
+                }
+                if (cur.right != null) {
+                    stack.push(cur.right);
+                }
+            }
+            // 左 右 头
+            while (!resStack.isEmpty()) {
+                System.out.print(resStack.pop().value + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        pre(root);
+        in(root);
+        pos(root);
+    }
+}
---
+++ basic/src/class11/Code6_SerializeAndReconstructTree.java
@@ -0,0 +1,201 @@
+package class11;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.concurrent.locks.StampedLock;
+
+/**
+ * 二叉树可以通过 先序、后序和按层遍历的方式序列化
+ * 无法通过中序的方式实现序列化和反序列化
+ */
+public class Code6_SerializeAndReconstructTree {
+
+    public static class TreeNode {
+        int value;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int value) {
+            this.value = value;
+        }
+    }
+
+    // 先序序列化
+    public static Queue<String> preSerial(TreeNode root) {
+        Queue<String> ans = new LinkedList<>();
+        pres(root, ans);
+        return ans;
+    }
+
+    private static void pres(TreeNode root, Queue<String> ans) {
+        if (root == null) {
+            ans.add(null);
+        } else {
+            ans.add(String.valueOf(root.value));
+            pres(root.left, ans);
+            pres(root.right, ans);
+        }
+    }
+
+    // 先序 反序列化
+    public static TreeNode buildByPreQueue(Queue<String> queue) {
+        if (queue.isEmpty()) {
+            return null;
+        }
+        return preb(queue);
+    }
+
+    private static TreeNode preb(Queue<String> queue) {
+        String value = queue.poll();
+        if (value == null) {
+            return null;
+        }
+        TreeNode head = new TreeNode(Integer.valueOf(value));
+        head.left = preb(queue);
+        head.right = preb(queue);
+        return head;
+    }
+
+    // 后序 序列化
+    public static Queue<String> posSerial(TreeNode head) {
+        Queue<String> ans = new LinkedList<>();
+        poss(head,ans);
+        return ans;
+
+    }
+
+    public static void poss(TreeNode head, Queue<String> ans) {
+        if (head == null) {
+            ans.add(null);
+        }else {
+            poss(head.left,ans);
+            poss(head.right,ans);
+            ans.add(String.valueOf(head.value));
+        }
+    }
+
+    // 后序 反序列化
+    public static TreeNode buildByPosSerial(Queue<String> queue) {
+        if (queue.isEmpty()) {
+            return null;
+        }
+        Stack<String> stack = new Stack<>();
+        // 左右头 -> 头右左
+        while (!queue.isEmpty()) {
+            stack.push(queue.poll());
+        }
+        return posb(stack);
+    }
+
+    private static TreeNode posb(Stack<String> stack) {
+        String value = stack.pop();
+        if (value == null) {
+            return null;
+        }
+        TreeNode head = new TreeNode(Integer.valueOf(value));
+        head.right= posb(stack);
+        head.left = posb(stack);
+        return head;
+    }
+
+    // 按层遍历 序列化
+    public static Queue<String> levelSerial(TreeNode head) {
+        Queue<String> ans = new LinkedList<>();
+        if (head == null) {
+            ans.add(null);
+        }else {
+            ans.add(String.valueOf(head.value));
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(head);
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                    ans.add(String.valueOf(node.left.value));
+                }else {
+                    ans.add(null);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                    ans.add(String.valueOf(node.right.value));
+                }else {
+                    ans.add(null);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static TreeNode buildByLevelSerial(Queue<String> levelList) {
+        if (levelList.isEmpty()) {
+            return null;
+        }
+        TreeNode head = generateNode(levelList.poll());
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(head);
+        TreeNode cur = null;
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            cur.left = generateNode(levelList.poll());
+            cur.right = generateNode(levelList.poll());
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+
+        return head;
+    }
+
+    public static TreeNode generateNode(String value) {
+        if (value == null) {
+            return null;
+        }
+        return new TreeNode(Integer.valueOf(value));
+    }
+
+    /*
+             1
+            / \
+          2    4
+         / \  / \
+      null 3 5  null
+     */
+    public static void main(String[] args) {
+        TreeNode head = new TreeNode(1);
+        head.left = new TreeNode(2);
+        head.left.right = new TreeNode(3);
+        head.right = new TreeNode(4);
+        head.right.left = new TreeNode(5);
+
+        // 先序
+        Queue<String> preSerial = preSerial(head);
+        printQueue(preSerial);
+        TreeNode root = buildByPreQueue(preSerial);
+
+        // 后序 序列化
+        Queue<String> posSerial = posSerial(head);
+        printQueue(posSerial);
+        TreeNode root2 = buildByPosSerial(posSerial);
+
+        // 按层遍历
+        Queue<String> levelList = levelSerial(head);
+        printQueue(levelList);
+        TreeNode root3 = buildByLevelSerial(levelList);
+        System.out.println();
+
+
+    }
+
+    public static void printQueue(Queue<String> queue) {
+        for (String s : queue) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+
+
+}

05389ec7417d348b4d3eaeb1f092af832e29ca11
zhangling <390597591@qq.com>
2022-11-13 23:55:18 +0800

    动态规划

--- src/Readme.java
+++ src/Readme.java
@@ -6,21 +6,26 @@ import basic.class10.*;
 public class Readme {
 
     /**
-     * <li> 冒泡排序 {@link basic.class01.BubbleSort} </li>
-     * <li> 插入排序 {@link basic.class01.InsertSort} </li>
-     * <li> 选择排序 {@link basic.class01.SelectSort} </li>
-     * <li> 快速排序 {@link basic.class03.Code04_QuickSort} </li>
-     * <li> 归并排序 {@link basic.class03.MergeSort} </li>
-     * <li> 堆排序 {@link basic.class04.Code03_HeapSort} </li>
-     * <li> 计数排序 {@link Code03_CountSort} </li>
-     * <li> 基数排序 {@link Code04_RadixSort} </li>
-     * <li> 二分法 {@link basic.class01.BSExist} {@link primary.class03.Code01_BSExist}</li>
+     * 排序算法
+     * <ol>
+     *      <li> 冒泡排序 {@link basic.class01.BubbleSort} </li>
+     *      <li> 插入排序 {@link basic.class01.InsertSort} </li>
+     *      <li> 选择排序 {@link basic.class01.SelectSort} </li>
+     *      <li> 快速排序 {@link basic.class03.Code04_QuickSort} </li>
+     *      <li> 归并排序 {@link basic.class03.MergeSort} </li>
+     *      <li> 堆排序 {@link basic.class04.Code03_HeapSort} </li>
+     *      <li> 计数排序 {@link Code03_CountSort} </li>
+     *      <li> 基数排序 {@link Code04_RadixSort} </li>
+     *      <li> 二分法 {@link basic.class01.BSExist} {@link primary.class03.Code01_BSExist}</li>
+     * </ol>
      */
     void sort() {}
 
     /**
      * 栈、队列、链表
+     * <ol>
      *
+     * </ol>
      * <li>　删除链表中给定值  {@link basic.class02.DeleteGivenValue}　</li>
      * <li>　反转链表  {@link ReverseList}　</li>
      * <li>　  {@link }　</li>
@@ -39,11 +44,13 @@ public class Readme {
     /**
      * 二叉树
      *
-     * <li> 遍历二查叉树 {@link Code01_TraversalBinaryTree} </li>
-     * <li> 非递归方式遍历二叉树 {@link Code01_UnRecursiveTraversalBT} </li>
-     * <li> 判断两棵树结构相同 {@link Code02_SameTree} </li>
-     * <li> 计算二叉树最大深度 {@link Code04_MaxDepthOfBinaryTree} </li>
-     * <li> 知道一棵二叉树的先序遍历和中序遍历，且先序遍历和中序遍历中没有重复值 {@link Code05_ConstructBinaryTreeFromPreorderAndInorderTraversal} </li>
+     * <ol>
+     *      <li> 遍历二查叉树 {@link basic.class11.Code01_TraversalBinaryTree} </li>
+     *      <li> 非递归方式遍历二叉树 {@link basic.class11.Code01_UnRecursiveTraversalBT} </li>
+     *      <li> 判断两棵树结构相同 {@link basic.class11.Code02_SameTree} </li>
+     *      <li> 计算二叉树最大深度 {@link basic.class11.Code04_MaxDepthOfBinaryTree} </li>
+     *      <li> 知道一棵二叉树的先序遍历和中序遍历，且先序遍历和中序遍历中没有重复值 {@link basic.class11.Code05_ConstructBinaryTreeFromPreorderAndInorderTraversal} </li>
+     * </ol>
      *
      */
     void binaryTree(){}
@@ -62,10 +69,36 @@ public class Readme {
 
     /**
      * 递归
+     *
      * <ol>
-     *     <li> 汉诺塔问题 {@link } </li>
+     *     <li> 汉诺塔问题 {@link basic.class18.Code02_Hanoi} </li>
+     *     <li> 打印一个字符串的全部子序列 {@link basic.class18.Code03_PrintAllSubsquences} </li>
+     *     <li> 打印一个字符串的全部排列 {@link basic.class18.Code04_PrintAllPermutations} </li>
+     *     <li> 不申请额外的数据结构，逆序这个栈 {@link basic.class18.Code05_ReverseStackUsingRecursive} </li>
      * </ol>
      *
      */
     void reverse(){}
+
+    /**
+     * 动态规划
+     *
+     * <ol>
+     *     <li> 机器人问题  {@link basic.class19.Code01_RobotWalk} </li>
+     *     <li> 取分数最大问题 {@link basic.class19.Code02_CardsInLine} </li>
+     *
+     *     <li> 背包问题 {@link basic.class20.Code01_Knapsack} </li>
+     *     <li> 数字字符串转换为字符 {@link basic.class20.Code02_ConvertToLetterString} </li>
+     *     <li> 贴纸问题 {@link basic.class20.Code03_StickersToSpellWord} </li>
+     *     <li> 最长公共子序列 {@link basic.class20.Code04_LongestCommonSubsequence} </li>
+     *
+     *     <li> 最长回文子序列 {@link basic.class21.Code01_PalindromeSubsequence} </li>
+     *     <li> 象棋中马跳到目标位置 {@link basic.class21.Code02_HorseJump} </li>
+     *     <li> 洗咖啡机问题 {@link basic.class21.Code03_Coffee} </li>
+     *
+     *     <li> 最短路径问题 {@link basic.class21.Code03_Coffee} </li>
+     *
+     * </ol>
+     */
+    void test(){}
 }
---
+++ src/basic/class21/Code01_PalindromeSubsequence.java
@@ -0,0 +1,141 @@
+package basic.class21;
+
+/**
+ * 最长回文子序列
+ *
+ * 给定一个字符串str，返回这个字符串的最长回文子序列长度
+ * 比如 ： str = “a12b3c43def2ghi1kpm”
+ * 最长回文子序列是“1234321”或者“123c321”，返回长度7
+ */
+// 测试链接：https://leetcode.com/problems/longest-palindromic-subsequence/
+public class Code01_PalindromeSubsequence {
+
+    /**
+     * 暴力递归
+     * @param s
+     * @return
+     */
+	public static int lpsl1(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+		char[] str = s.toCharArray();
+		return f(str, 0, str.length - 1);
+	}
+
+	// str[L..R]最长回文子序列长度返回
+	public static int f(char[] str, int L, int R) {
+		if (L == R) {
+			return 1;
+		}
+		if (L == R - 1) {
+			return str[L] == str[R] ? 2 : 1;
+		}
+        // 不以 L 开头，和 R 结尾
+		int p1 = f(str, L + 1, R - 1);
+        // L 开头，不以 R 结尾
+		int p2 = f(str, L, R - 1);
+		int p3 = f(str, L + 1, R);
+        // 以 L 开头和 R 结尾，必须 L，R位置的值相等
+		int p4 = str[L] != str[R] ? 0 : (2 + f(str, L + 1, R - 1));
+		return Math.max(Math.max(p1, p2), Math.max(p3, p4));
+	}
+
+    /**
+     * 动态规划
+     * @param s
+     * @return
+     */
+	public static int lpsl2(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+		char[] str = s.toCharArray();
+		int N = str.length;
+		int[][] dp = new int[N][N];
+		dp[N - 1][N - 1] = 1;
+		for (int i = 0; i < N - 1; i++) {
+			dp[i][i] = 1;
+			dp[i][i + 1] = str[i] == str[i + 1] ? 2 : 1;
+		}
+		for (int L = N - 3; L >= 0; L--) {
+			for (int R = L + 2; R < N; R++) {
+				dp[L][R] = Math.max(dp[L][R - 1], dp[L + 1][R]);
+				if (str[L] == str[R]) {
+					dp[L][R] = Math.max(dp[L][R], 2 + dp[L + 1][R - 1]);
+				}
+			}
+		}
+		return dp[0][N - 1];
+	}
+
+	public static int longestPalindromeSubseq1(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+		if (s.length() == 1) {
+			return 1;
+		}
+		char[] str = s.toCharArray();
+		char[] reverse = reverse(str);
+		return longestCommonSubsequence(str, reverse);
+	}
+
+	public static char[] reverse(char[] str) {
+		int N = str.length;
+		char[] reverse = new char[str.length];
+		for (int i = 0; i < str.length; i++) {
+			reverse[--N] = str[i];
+		}
+		return reverse;
+	}
+
+	public static int longestCommonSubsequence(char[] str1, char[] str2) {
+		int N = str1.length;
+		int M = str2.length;
+		int[][] dp = new int[N][M];
+		dp[0][0] = str1[0] == str2[0] ? 1 : 0;
+		for (int i = 1; i < N; i++) {
+			dp[i][0] = str1[i] == str2[0] ? 1 : dp[i - 1][0];
+		}
+		for (int j = 1; j < M; j++) {
+			dp[0][j] = str1[0] == str2[j] ? 1 : dp[0][j - 1];
+		}
+		for (int i = 1; i < N; i++) {
+			for (int j = 1; j < M; j++) {
+				dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+				if (str1[i] == str2[j]) {
+					dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
+				}
+			}
+		}
+		return dp[N - 1][M - 1];
+	}
+
+	public static int longestPalindromeSubseq2(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+		if (s.length() == 1) {
+			return 1;
+		}
+		char[] str = s.toCharArray();
+		int N = str.length;
+		int[][] dp = new int[N][N];
+		dp[N - 1][N - 1] = 1;
+		for (int i = 0; i < N - 1; i++) {
+			dp[i][i] = 1;
+			dp[i][i + 1] = str[i] == str[i + 1] ? 2 : 1;
+		}
+		for (int i = N - 3; i >= 0; i--) {
+			for (int j = i + 2; j < N; j++) {
+				dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+				if (str[i] == str[j]) {
+					dp[i][j] = Math.max(dp[i][j], dp[i + 1][j - 1] + 2);
+				}
+			}
+		}
+		return dp[0][N - 1];
+	}
+
+}
